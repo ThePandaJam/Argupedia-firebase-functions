ff004a927a1ba00350f59e3a4a91b20b290d7a2b
ThePandaJam <veronika.kravchenko@barbal.co>
2021-09-14 23:13:12 +0100

    removed console logs

--- functions/util/fbAuth.js
+++ functions/util/fbAuth.js
@@ -20,7 +20,6 @@ module.exports = (req, res, next) => {
         .verifyIdToken(idToken)
         .then(decodedToken => {
             req.user = decodedToken;
-            console.log(decodedToken)
             return db.collection('users')
                 .where('userId', '==', req.user.uid)
                 .limit(1)
